f402aa29838d6d73ad1f4df7e9109a0680998704
Soc Virnyl S. Estela <66054069+ReneganRonin@users.noreply.github.com>
2021-04-13 15:03:11 +0800

    Lua conversion in progress

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,3 +1,11 @@
+-- Vim Slime
+vim.g.slime_target = "kitty"
+vim.g.slime_default_config = {window_id= 1, listen_on= os.getenv('KITTY_LISTEN_ON')}
 
-vim.cmd('let g:slime_target = "kitty"')
-vim.cmd('let g:slime_default_config = {"window_id": 1, "listen_on": $KITTY_LISTEN_ON}')
+-- Completions Neovim
+vim.g.completion_enable_auto_hover = 1
+vim.g.completion_enable_auto_popup = 1
+
+-- Diagnostics
+vim.g.diagnostic_enable_popup_while_jump = 1
+vim.g.diagnostic_enable_virtual_text = 0
