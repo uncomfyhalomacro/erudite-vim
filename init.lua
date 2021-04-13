62b50e1aa675b22494f9a3a8a5e12028c048aa3b
Soc Virnyl S. Estela <66054069+ReneganRonin@users.noreply.github.com>
2021-04-13 10:04:27 +0800

    Update logo

--- init.lua
+++ init.lua
@@ -9,7 +9,16 @@ require('lspsaga.codeaction').range_code_action()
 -- Themes
 require('colorscheme')
 
--- Settings 
+-- Settings
 require('settings')
 -- Using vimscript for now
 vim.cmd('source ~/.config/nvim/vimscripts/settings.vim')
+
+-- Dashboard Theme
+-- ugly though. I should create a predetermined color in the future
+--vim.g.dashboard_preview_command = 'lolcat --spread=2.0 --seed=23'
+--vim.g.dashboard_preview_file = '~/.config/nvim/dashboardlogo.cat'
+--vim.g.dashboard_preview_file_height = 15
+--vim.g.dashboard_preview_file_width = 95
+
+
