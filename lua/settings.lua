b0e58d44af0c072d90cd7b0cf8038d9980ef609a
Soc Virnyl S. Estela <66054069+ReneganRonin@users.noreply.github.com>
2021-04-15 09:23:41 +0800

    Convert to lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,9 +2,9 @@
 vim.cmd("filetype plugin on")
 vim.o.hidden = true
 vim.o.splitbelow = true
-vim.o.tabstop = 4
-vim.o.mouse = 'nvira'
-vim.o.shiftwidth = 4
+vim.cmd('set ts=4')
+vim.o.mouse = 'a'
+vim.cmd('set sw=4')
 vim.wo.number = true
 vim.wo.cursorline = true
 vim.o.cmdheight = 2
@@ -21,8 +21,42 @@ vim.cmd('set shortmess+=c')
 vim.o.termguicolors = true
 
 -- Set autosave last session
-vim.cmd('autocmd BufWritePost * SessionSave')
+vim.cmd('autocmd BufWritePost * silent! SessionSave')
 
 -- Diagnostics
 vim.g.diagnostic_enable_popup_while_jump = 1
 vim.g.diagnostic_enable_virtual_text = 0
+
+-- Keymappings will be here for now
+-- Explorer
+vim.api.nvim_set_keymap('n', '<Space>f', ':CocCommand explorer --preset floating<CR>', {noremap= false, silent = false})
+vim.api.nvim_set_keymap('n', '<Space>e', ':CocCommand explorer<CR>', {noremap = false, silent = false})
+vim.api.nvim_set_keymap('n', '<leader>e', ':CocCommand explorer<CR>', {noremap = true, silent = true})
+vim.cmd('autocmd BufEnter * if (winnr(\'$\') == 1 && &filetype == \'coc-explorer\') | q | endif')
+vim.g.coc_explorer_global_presets = {
+    floating = {
+	position = 'floating'
+    },
+    floatingLeftside = {
+	position = 'floating',
+	['floating-position'] = 'left-center',
+	['floating-width'] = 30
+    },
+    floatingRightside = {
+	position = 'floating',
+	['floating-position'] = 'right-center',
+	['floating-width'] = 30
+    },
+    simplify = {
+	['file.child.template'] = '[selection | clip | 1] [indent][icon | 1] [filename omitCenter 1]'
+    }
+}
+
+-- Dashboard
+vim.api.nvim_set_keymap('n', '<Space>h', ':DashboardFindHistory<CR>', {noremap= false, silent = false})
+vim.api.nvim_set_keymap('n', '<Space>F', ':DashboardFindFile<CR>', {noremap= false, silent = false})
+vim.api.nvim_set_keymap('n', '<Space>l', ':SessionLoad<CR>', {noremap= false, silent = false})
+vim.api.nvim_set_keymap('n', '<Space>n', ':DashboardNewFile<CR>', {noremap= false, silent = false})
+
+-- Formatting
+vim.api.nvim_set_keymap('n', '<Space>k', ':lua vim.lsp.buf.formatting()', {noremap = false, silent = false})
--- mappings.vim
+++ mappings.vim
@@ -41,29 +41,29 @@ let g:netrw_browse_split = 3
 let g:netrw_altv = 1
 let g:netrw_winsize = 25
 " Explorer
-let g:coc_explorer_global_presets = {
-\   'floating': {
-\      'position': 'floating',
-\   },
-\   'floatingLeftside': {
-\      'position': 'floating',
-\      'floating-position': 'left-center',
-\      'floating-width': 30,
-\   },
-\   'floatingRightside': {
-\      'position': 'floating',
-\      'floating-position': 'right-center',
-\      'floating-width': 30,
-\   },
-\   'simplify': {
-\     'file.child.template': '[selection | clip | 1] [indent][icon | 1] [filename omitCenter 1]'
-\   }
-\ }
-map <silent> <space>e :CocCommand explorer<CR>
-nnoremap <silent> <leader>e :CocCommand explorer<CR>
-nmap <space>f :CocCommand explorer --preset floatingRightside<CR>
-autocmd BufEnter * if (winnr("$") == 1 && &filetype == 'coc-explorer') | q | endif
-nmap <space>k <cmd>lua vim.lsp.buf.formatting()<CR>
+"let g:coc_explorer_global_presets = {
+"\   'floating': {
+"\      'position': 'floating',
+"\   },
+"\   'floatingLeftside': {
+"\      'position': 'floating',
+"\      'floating-position': 'left-center',
+"\      'floating-width': 30,
+"\   },
+"\   'floatingRightside': {
+"\      'position': 'floating',
+"\      'floating-position': 'right-center',
+"\      'floating-width': 30,
+"\   },
+"\   'simplify': {
+"\     'file.child.template': '[selection | clip | 1] [indent][icon | 1] [filename omitCenter 1]'
+"\   }
+"\ }
+"map <silent> <space>e :CocCommand explorer<CR>
+"nnoremap <silent> <leader>e :CocCommand explorer<CR>
+"nmap <space>f :CocCommand explorer --preset floatingRightside<CR>
+"autocmd BufEnter * if (winnr("$") == 1 && &filetype == 'coc-explorer') | q | endif
+"nmap <space>k <cmd>lua vim.lsp.buf.formatting()<CR>
 
 " Snippets
 " Use <C-l> for trigger snippet expand.
