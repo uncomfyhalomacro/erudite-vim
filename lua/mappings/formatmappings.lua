7e7b6844fa13fc8af338dc3562df80056ef33bb5
Soc Virnyl S. Estela <66054069+ReneganRonin@users.noreply.github.com>
2021-04-16 15:29:53 +0800

    Add kitty bindings to whichkey

--- lua/mappings/formatmappings.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_set_keymap('n', '<Space>k', ':lua vim.lsp.buf.formatting()', {noremap = false, silent = false})
-vim.api.nvim_set_keymap('n', '<Space>gr', ':Lspsaga rename<CR>', {noremap = true, silent = false})
