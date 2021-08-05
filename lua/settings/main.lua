a53e73d722aaa1f112609ab0ab0b92739d9f777d
Soc Virnyl S. Estela <66054069+ReneganRonin@users.noreply.github.com>
2021-08-05 10:57:27 +0800

    Add guifont and a setting for Neovide

--- lua/settings/main.lua
+++ lua/settings/main.lua
@@ -10,23 +10,22 @@ vim.cmd('let &t_ut=\'\'')
 vim.opt.shortmess:append('c')
 vim.opt.ts = 4
 vim.opt.sw = 4
-vim.o.backupdir = os.getenv('HOME').."/.local/share/nvim/backup/,/tmp//"
 vim.o.cmdheight = 2
 vim.o.completefunc = "emoji#complete"
 vim.o.completeopt = "menuone,noinsert,noselect"
-vim.o.directory = os.getenv('HOME').."/.local/share/nvim/swap/,/tmp//"
 vim.o.hidden = true
 vim.opt.mouse = 'a'
 vim.o.showtabline = 1
 vim.o.splitbelow = true
 vim.o.termguicolors = true
-vim.o.undodir = os.getenv('HOME').."/.local/share/nvim/undo/, /tmp//"
 vim.o.updatetime = 100
 vim.wo.cursorline = true
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.o.timeoutlen = 1000
 vim.o.timeout = true
+vim.o.guifont = "JuliaMono,CaskaydiaCove Nerd Font:h12"
+vim.g.neovide_cursor_vfx_mode = "torpedo"
 -- Diagnostics
 vim.g.diagnostic_enable_popup_while_jump = 1
 vim.g.diagnostic_enable_virtual_text = 0
