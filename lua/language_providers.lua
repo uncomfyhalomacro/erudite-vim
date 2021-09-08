bb42a01e2e2a4a7c60d76ffac92f1f65d4bda928
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2021-09-08 09:42:38 +0800

    Making it somewhat easy when migrating to windows

--- lua/language_providers.lua
+++ lua/language_providers.lua
@@ -1,6 +1,6 @@
 -- Python
 if os.getenv("HOME") == nil then
-    vim.g.python3_host_prog = "/c/Users/uncomfyhalomacro/scoop/apps/python/current/python"
+    vim.g.python3_host_prog = "C:/Users/uncomfyhalomacro/scoop/apps/python/current/python"
 else
     vim.g.python3_host_prog = "/usr/bin/python"
     vim.g.python_host_prog = "/usr/bin/python2" -- python2 is dead dream on bro
