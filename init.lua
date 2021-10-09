b5ba343a40f0bfc4f5eea02bc3116845a5a0cdad
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2021-10-09 18:15:01 +0800

    Add newlines

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
 local impatient_path=vim.fn.stdpath("data") .. "/site/pack/packer/start/impatient.nvim"
-
 if vim.fn.empty(vim.fn.glob(impatient_path)) > 0 then
 	require('impatient')
 end
+
 require('plugins')
 require('settings')
+
 vim.o.guifont="JuliaMono:h10"
+
 if os.getenv("HOME") ~= nil then
 	vim.cmd "autocmd BufWritePost plugins.lua PackerCompile"
 	vim.cmd "autocmd BufWinEnter * NvimBlamerAuto"
