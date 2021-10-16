de14ba889b26d1f6ec6b2b49a32cc2c0090c2679
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2021-10-16 14:59:23 +0800

    Fix impatient in init.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,7 +5,7 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-	execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+	execute("!git clone https://github.com/wbthomason/packer.nvim --depth=1 " .. install_path)
 	execute("packadd packer.nvim")
 end
 
