f7cd71176a22d6b807f24ff099802cd0ac9d964d
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-01-20 10:59:04 +0800

    Fix tmux and kitty command

--- lua/core/keymaps/kitty.lua
+++ lua/core/keymaps/kitty.lua
@@ -1,8 +1,8 @@
 if vim.fn.getenv("TERM") == "xterm-kitty" and vim.fn.getenv("TERM_PROGRAM") == vim.NIL then
 	local kitty = {
 		name = "+terminal",
-		k = { ":!kitty @new-window --window-type os --cwd $PWD<CR>:<C-[><CR>", "open terminal here" },
-		s = { ":!kitty @new-window --cwd $PWD<CR>:<C-[><CR>", "split-terminal" },
+		k = { ":!kitty @new-window --window-type os --cwd %:p:h <CR>:<C-[><CR>", "open terminal here" },
+		s = { ":!kitty @new-window --cwd %:p:h <CR>:<C-[><CR>", "split-terminal" },
 		-- going to add ghpc or whatever haskell
 	}
 
@@ -11,7 +11,7 @@ elseif vim.fn.getenv("TERM_PROGRAM") == "tmux" then
 	local tmux = {
 		name = "+tmux",
 		k = { ":!tmux split-window<CR>:<C-[><CR>", "split current tmux-window" },
-		s = { ":!tmux split-window" .. ' -c "' .. vim.fn.getcwd() .. '"<CR>:<C-[><CR>', "split tmux-window on cwd" },
+		s = { ":!tmux split-window" .. ' -c ' .. "%:p:h" .. ' <CR>:<C-[><CR>', "split tmux-window on cwd" },
 	}
 	return tmux
 else
