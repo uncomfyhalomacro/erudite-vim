69536ee254cedb1fbed7c798c7381283a560f95d
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-01-22 08:48:32 +0800

    Change scroll docs mappings to default

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -37,7 +37,7 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 		["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 		["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
