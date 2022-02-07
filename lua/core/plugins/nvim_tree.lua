9ce02fe445344b2590272a505b4326aea925e8a2
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-02-07 17:28:05 +0800

    Update nvim_tree

--- lua/core/plugins/nvim_tree.lua
+++ lua/core/plugins/nvim_tree.lua
@@ -1,10 +1,23 @@
+vim.g.nvim_tree_respect_buf_cwd = 1
 require("nvim-tree").setup({
+	open_on_startup = false,
 	update_cwd = true,
+	--respect_buf_cwd = 1,
 	update_focused_file = {
 		enable = true,
-		update_cwd = true,
 	},
 	diagnostics = {
 		enable = true,
+	},
+	actions = {
+		change_dir = {
+			global = true,
+		},
+		open_file = {
+			quit_on_open = false,
+		},
+	},
+	view = {
+		hide_root_folder = false,
 	},
 })
