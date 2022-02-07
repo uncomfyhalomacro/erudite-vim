0c6186b1ea30b200657ed0d2616e93f6f2bf2f59
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-02-07 17:27:30 +0800

    Update transparent

--- lua/core/plugins/transparent.lua
+++ lua/core/plugins/transparent.lua
@@ -1,5 +1,5 @@
 require("transparent").setup({
-	exclude = { -- table/string: additional groups that should be clear
+	extra_groups = { -- table/string: additional groups that should be clear
 		-- In particular, when you set it to 'all', that means all avaliable groups
 
 		-- example of akinsho/nvim-bufferline.lua
