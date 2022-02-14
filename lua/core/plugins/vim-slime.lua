fab0ef7de24a26cf91fb55d2dff35cd4c0331006
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-02-14 09:39:35 +0800

    Fix vim-slime config

--- lua/core/plugins/vim-slime.lua
+++ lua/core/plugins/vim-slime.lua
@@ -1,9 +1,11 @@
 -- Check TERM
-if os.getenv("TERM") == "xterm-kitty" then
+if vim.fn.getenv("TERM") == "xterm-kitty" and vim.fn.getenv("TERM_PROGRAM") == vim.NIL then
 	vim.g.slime_target = "kitty"
 	vim.g.slime_default_config = { window_id = 1, listen_on = os.getenv("KITTY_LISTEN_ON") }
-else
+elseif vim.fn.getenv("TERM_PROGRAM") == "tmux" then
 	vim.g.slime_target = "tmux"
 	vim.g.slime_paste_file = "$HOME/.slime_paste"
 	vim.cmd([[let g:slime_default_config = {"socket_name": "default", "target_pane": "{last}"}]])
+else
+  vim.g.slime_target = "screen"  -- as described in vim-slime's readme, screen is the default
 end
