9c6d6bc7c28b1628c6fd1a5dfd1484ae3492af3e
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-02-14 21:18:24 +0800

    Update which-key user-interface

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -241,7 +241,7 @@ local plugins = function(use)
 					scroll_up = "<c-u>", -- binding to scroll up inside the popup
 				},
 				window = {
-					border = "none", -- none, single, double, shadow
+					border = "single", -- none, single, double, shadow
 					position = "bottom", -- bottom, top
 					margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
 					padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
@@ -249,7 +249,7 @@ local plugins = function(use)
 				},
 				layout = {
 					height = { min = 4, max = 25 }, -- min and max height of the columns
-					width = { min = 20, max = 50 }, -- min and max width of the columns
+					width = { min = 20, max = 120 }, -- min and max width of the columns
 					spacing = 25, -- spacing between columns
 					align = "center", -- align columns left, center or right
 				},
