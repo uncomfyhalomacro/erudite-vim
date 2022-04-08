baa39caec84f4131592c03c466a3d1e09f2d6c8a
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-04-08 17:58:52 +0800

    Simplify and improve julia project detection

--- lua/core/plugins/lspsaga.lua
+++ lua/core/plugins/lspsaga.lua
@@ -101,11 +101,22 @@ local servers = {
 	julials = {
 		cmd = {
 			"julia",
-			"-J".. vim.fn.getenv("HOME") .. "/.julia/environments/nvim-lspconfig/languageserver.so",
+			"-J" .. vim.fn.getenv("HOME") .. "/.julia/environments/nvim-lspconfig/languageserver.so",
 			"--startup-file=no",
 			"--history-file=no",
 			"-e",
-			'    # Load LanguageServer.jl: attempt to load from ~/.julia/environments/nvim-lspconfig\n    # with the regular load path as a fallback\n    ls_install_path = joinpath(\n        get(DEPOT_PATH, 1, joinpath(homedir(), ".julia")),\n        "environments", "nvim-lspconfig"\n    )\n    pushfirst!(LOAD_PATH, ls_install_path)\n    using LanguageServer\n    popfirst!(LOAD_PATH)\n    depot_path = get(ENV, "JULIA_DEPOT_PATH", "")\n    project_path = let\n        dirname(something(\n            ## 1. Finds an explicitly set project (JULIA_PROJECT)\n            Base.load_path_expand((\n                p = get(ENV, "JULIA_PROJECT", nothing);\n                p === nothing ? nothing : isempty(p) ? nothing : p\n            )),\n            ## 2. Look for a Project.toml file in the current working directory,\n            ##    or parent directories, with $HOME as an upper boundary\n            Base.current_project(),\n            ## 3. First entry in the load path\n            get(Base.load_path(), 1, nothing),\n            ## 4. Fallback to default global environment,\n            ##    this is more or less unreachable\n            Base.load_path_expand("@v#.#"),\n        ))\n    end\n    @info "Running language server" VERSION pwd() project_path depot_path\n    server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)\n    server.runlinter = true\n    run(server)\n  ',
+			[[
+    ls_install_path = joinpath(get(DEPOT_PATH, 1, joinpath(homedir(), ".julia")), "environments", "nvim-lspconfig");
+    pushfirst!(LOAD_PATH, ls_install_path);
+    using LanguageServer;
+    popfirst!(LOAD_PATH);
+    depot_path = get(ENV, "JULIA_DEPOT_PATH", "");
+    buffer_file = "]] .. vim.api.nvim_buf_get_name(0) .. '";' .. [[
+    project_path = dirname(something(Base.current_project(dirname(buffer_file)), Base.active_project()));
+    server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path);
+    server.runlinter = true;
+    run(server);
+		]],
 		},
 		settings = {
 			julia = {
