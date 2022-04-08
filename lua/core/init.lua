ecd805645dea7be1522b74a669402181f3974ff4
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-04-09 00:30:04 +0800

    Update colorscheme to respect screenshots in README

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -8,7 +8,7 @@ require("core.keymaps") -- keymaps should always be last
 -- IMPATIENT STARTS HERE --
 -- Reporting a startup time of less than 3.0 milliseconds --
 vim.o.background = "light"
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme catppuccin")
 vim.g.transparent_enabled = false
 vim.g.presence_auto_update = true
 vim.o.guifont = "JuliaMono,Hack Nerd Font,Noto Color Emoji:h10"
