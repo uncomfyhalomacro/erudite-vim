c2b365d32730733bee05c05292b1c5b7fc0f5ae8
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-04-20 20:18:15 +0800

    Testing unmap and map with autocmd

--- lua/core/keymaps/init.lua
+++ lua/core/keymaps/init.lua
@@ -13,6 +13,9 @@ local function map(mode, lhs, rhs, opts)
 	end
 	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
+local function unmap(mode, rhs)
+	vim.api.nvim_del_keymap(mode, rhs)
+end
 
 -- LEADER PREFIX --
 -- Basic stuff
@@ -53,38 +56,210 @@ local hop_key_extend = {
 }
 
 hop_key.e = hop_key_extend
-function GitKeys()
+
+function git_automap()
 	local check_if_local_git_repo = vim.fn.system({
 		"git",
 		"-C",
-		vim.fn.getcwd(),
+		vim.fn.expand("%:p:h"),
 		"rev-parse",
 		"--is-inside-work-tree",
 	})
 
-	local git_keys = {
-		name = "+git",
-		a = { ":Git add %<CR>", "git add current file" },
-		s = { ":Gitsigns blame_line<CR>", "show fancy git blame" },
-		t = { ":Gitsigns toggle_current_blame_line<CR>", "toggle inline git blame" },
-		p = { ":Git push<CR>", "push commited changes" },
-		S = { ":Git status<CR>", "show git status" },
-		c = { ":Git commit<CR>", "commit staged" },
-		f = { ":Git fetch<CR>", "fetch changes from remote" },
-		C = { ":Telescope git_commits<CR>", "select a specific commit" },
-		u = { ":Telescope git_status<CR>", "select git file from git status" },
-		F = { ":Telescope git_files<CR>", "select files that are tracked" },
-		b = { ":Telescope git_branches<CR>", "select a specific branch" },
-	}
-
-	if string.find(check_if_local_git_repo, "true") then -- this is because there are weird characters returned e.g. "true^@" instead of just "true" which is kinda annoying
-		keymap.g = git_keys
-		wk.register({ ["<leader>"] = keymap })
-		wk.register({ ["<localleader>"] = local_keymap })
+	-- this is because there are weird characters returned e.g. "true^@" instead of just "true" which is kinda annoying
+	if string.find(check_if_local_git_repo, "true") then
+		local git_opts = { silent = true }
+		map("n", "<leader>ga", ":Git add %<CR>", git_opts)
+		map("n", "<leader>gs", ":Gitsigns blame_line<CR>", git_opts)
+		map("n", "<leader>gt", ":Gitsigns toggle_current_blame_line<CR>", git_opts)
+		map("n", "<leader>gp", ":Git push <CR>", git_opts)
+		map("n", "<leader>gS", ":Git status <CR>", git_opts)
+		map("n", "<leader>gc", ":Git commit <CR>", git_opts)
+		map("n", "<leader>gf", ":Git fetch <CR>", git_opts)
+		map("n", "<leader>gC", ":Telescope git_commits <CR>", git_opts)
+		map("n", "<leader>gu", ":Telescope git_status <CR>", git_opts)
+		map("n", "<leader>gF", ":Telescope git_files <CR>", git_opts)
+		map("n", "<leader>gb", ":Telescope git_branches <CR>", git_opts)
+		-- local git_keys = {
+		-- 	name = "+git",
+		-- 	a = { ":Git add %<CR>", "git add current file" },
+		-- 	s = { ":Gitsigns blame_line<CR>", "show fancy git blame" },
+		-- 	t = { ":Gitsigns toggle_current_blame_line<CR>", "toggle inline git blame" },
+		-- 	p = { ":Git push<CR>", "push commited changes" },
+		-- 	S = { ":Git status<CR>", "show git status" },
+		-- 	c = { ":Git commit<CR>", "commit staged" },
+		-- 	f = { ":Git fetch<CR>", "fetch changes from remote" },
+		-- 	C = { ":Telescope git_commits<CR>", "select a specific commit" },
+		-- 	u = { ":Telescope git_status<CR>", "select git file from git status" },
+		-- 	F = { ":Telescope git_files<CR>", "select files that are tracked" },
+		-- 	b = { ":Telescope git_branches<CR>", "select a specific branch" },
+		-- }
+		-- keymap.g = git_keys
+		-- wk.register({ ["<leader>"] = keymap })
+		-- wk.register({ ["<localleader>"] = local_keymap })
+		wk.setup({
+			plugins = {
+				marks = true, -- shows a list of your marks on ' and `
+				registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+				spelling = {
+					enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+					suggestions = 20, -- how many suggestions should be shown in the list?
+				},
+				-- the presets plugin, adds help for a bunch of default keybindings in Neovim
+				-- No actual key bindings are created
+				presets = {
+					operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+					motions = true, -- adds help for motions
+					text_objects = true, -- help for text objects triggered after entering an operator
+					windows = true, -- default bindings on <c-w>
+					nav = true, -- misc bindings to work with windows
+					z = true, -- bindings for folds, spelling and others prefixed with z
+					g = true, -- bindings for prefixed with g
+				},
+			},
+			-- add operators that will trigger motion and text object completion
+			-- to enable all native operators, set the preset / operators plugin above
+			operators = { gc = "Comments" },
+			key_labels = {
+				-- override the label used to display some keys. It doesn't effect WK in any other way.
+				-- For example:
+				-- ["<space>"] = "SPC",
+				-- ["<cr>"] = "RET",
+				-- ["<tab>"] = "TAB",
+			},
+			icons = {
+				breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+				separator = "➜", -- symbol used between a key and it's label
+				group = "+", -- symbol prepended to a group
+			},
+			popup_mappings = {
+				scroll_down = "<c-d>", -- binding to scroll down inside the popup
+				scroll_up = "<c-u>", -- binding to scroll up inside the popup
+			},
+			window = {
+				border = "single", -- none, single, double, shadow
+				position = "bottom", -- bottom, top
+				margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+				padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+				winblend = 0,
+			},
+			layout = {
+				height = { min = 4, max = 25 }, -- min and max height of the columns
+				width = { min = 20, max = 200 }, -- min and max width of the columns
+				spacing = 25, -- spacing between columns
+				align = "center", -- align columns left, center or right
+			},
+			ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+			show_help = true, -- show help message on the command line when the popup is visible
+			triggers = { "<leader>", "<localleader>", "g", "<Shift>", "[", "]" }, -- automatically setup triggers
+			-- triggers = {"<leader>"} -- or specify a list manually
+			triggers_blacklist = {
+				-- list of mode / prefixes that should never be hooked by WhichKey
+				-- this is mostly relevant for key maps that start with a native binding
+				-- most people should not need to change this
+				i = { "j", "k" },
+				v = { "j", "k" },
+			},
+			hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+		})
+	else
+		-- trickery
+		local git_opts = { silent = true }
+		map("n", "<leader>ga", ":Git add %<CR>", git_opts)
+		map("n", "<leader>gs", ":Gitsigns blame_line<CR>", git_opts)
+		map("n", "<leader>gt", ":Gitsigns toggle_current_blame_line<CR>", git_opts)
+		map("n", "<leader>gp", ":Git push <CR>", git_opts)
+		map("n", "<leader>gS", ":Git status <CR>", git_opts)
+		map("n", "<leader>gc", ":Git commit <CR>", git_opts)
+		map("n", "<leader>gf", ":Git fetch <CR>", git_opts)
+		map("n", "<leader>gC", ":Telescope git_commits <CR>", git_opts)
+		map("n", "<leader>gu", ":Telescope git_status <CR>", git_opts)
+		map("n", "<leader>gF", ":Telescope git_files <CR>", git_opts)
+		map("n", "<leader>gb", ":Telescope git_branches <CR>", git_opts)
+		unmap("n", "<leader>ga")
+		unmap("n", "<leader>gs")
+		unmap("n", "<leader>gt")
+		unmap("n", "<leader>gp")
+		unmap("n", "<leader>gS")
+		unmap("n", "<leader>gc")
+		unmap("n", "<leader>gf")
+		unmap("n", "<leader>gC")
+		unmap("n", "<leader>gu")
+		unmap("n", "<leader>gF")
+		unmap("n", "<leader>gb")
+		wk.setup({
+			plugins = {
+				marks = true, -- shows a list of your marks on ' and `
+				registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+				spelling = {
+					enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+					suggestions = 20, -- how many suggestions should be shown in the list?
+				},
+				-- the presets plugin, adds help for a bunch of default keybindings in Neovim
+				-- No actual key bindings are created
+				presets = {
+					operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+					motions = true, -- adds help for motions
+					text_objects = true, -- help for text objects triggered after entering an operator
+					windows = true, -- default bindings on <c-w>
+					nav = true, -- misc bindings to work with windows
+					z = true, -- bindings for folds, spelling and others prefixed with z
+					g = true, -- bindings for prefixed with g
+				},
+			},
+			-- add operators that will trigger motion and text object completion
+			-- to enable all native operators, set the preset / operators plugin above
+			operators = { gc = "Comments" },
+			key_labels = {
+				-- override the label used to display some keys. It doesn't effect WK in any other way.
+				-- For example:
+				-- ["<space>"] = "SPC",
+				-- ["<cr>"] = "RET",
+				-- ["<tab>"] = "TAB",
+			},
+			icons = {
+				breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+				separator = "➜", -- symbol used between a key and it's label
+				group = "+", -- symbol prepended to a group
+			},
+			popup_mappings = {
+				scroll_down = "<c-d>", -- binding to scroll down inside the popup
+				scroll_up = "<c-u>", -- binding to scroll up inside the popup
+			},
+			window = {
+				border = "single", -- none, single, double, shadow
+				position = "bottom", -- bottom, top
+				margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+				padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+				winblend = 0,
+			},
+			layout = {
+				height = { min = 4, max = 25 }, -- min and max height of the columns
+				width = { min = 20, max = 200 }, -- min and max width of the columns
+				spacing = 25, -- spacing between columns
+				align = "center", -- align columns left, center or right
+			},
+			ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+			show_help = true, -- show help message on the command line when the popup is visible
+			triggers = { "<leader>", "<localleader>", "g", "<Shift>", "[", "]" }, -- automatically setup triggers
+			-- triggers = {"<leader>"} -- or specify a list manually
+			triggers_blacklist = {
+				-- list of mode / prefixes that should never be hooked by WhichKey
+				-- this is mostly relevant for key maps that start with a native binding
+				-- most people should not need to change this
+				i = { "j", "k" },
+				v = { "j", "k" },
+			},
+			hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ ", "<leader>g" }, -- hide mapping boilerplate
+		})
 	end
 end
-
-vim.cmd("autocmd BufEnter * :lua GitKeys()")
+vim.api.nvim_exec(
+	[[
+			autocmd CursorMoved,CursorMovedI <buffer> :lua git_automap()
+]],
+	true
+)
 
 local zk_keys = {
 	name = "+zettelkasten",
@@ -154,6 +329,72 @@ vim.cmd("autocmd FileType lua :lua require('core.keymaps.lua')")
 vim.cmd("autocmd FileType markdown :lua require('core.keymaps.markdown')")
 vim.cmd("autocmd FileType tex :lua require('core.keymaps.texlab')")
 vim.cmd("autocmd FileType bib :lua require('core.keymaps.texlab')")
+
+wk.setup({
+	plugins = {
+		marks = true, -- shows a list of your marks on ' and `
+		registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+		spelling = {
+			enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+			suggestions = 20, -- how many suggestions should be shown in the list?
+		},
+		-- the presets plugin, adds help for a bunch of default keybindings in Neovim
+		-- No actual key bindings are created
+		presets = {
+			operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+			motions = true, -- adds help for motions
+			text_objects = true, -- help for text objects triggered after entering an operator
+			windows = true, -- default bindings on <c-w>
+			nav = true, -- misc bindings to work with windows
+			z = true, -- bindings for folds, spelling and others prefixed with z
+			g = true, -- bindings for prefixed with g
+		},
+	},
+	-- add operators that will trigger motion and text object completion
+	-- to enable all native operators, set the preset / operators plugin above
+	operators = { gc = "Comments" },
+	key_labels = {
+		-- override the label used to display some keys. It doesn't effect WK in any other way.
+		-- For example:
+		-- ["<space>"] = "SPC",
+		-- ["<cr>"] = "RET",
+		-- ["<tab>"] = "TAB",
+	},
+	icons = {
+		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+		separator = "➜", -- symbol used between a key and it's label
+		group = "+", -- symbol prepended to a group
+	},
+	popup_mappings = {
+		scroll_down = "<c-d>", -- binding to scroll down inside the popup
+		scroll_up = "<c-u>", -- binding to scroll up inside the popup
+	},
+	window = {
+		border = "single", -- none, single, double, shadow
+		position = "bottom", -- bottom, top
+		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+		padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+		winblend = 0,
+	},
+	layout = {
+		height = { min = 4, max = 25 }, -- min and max height of the columns
+		width = { min = 20, max = 200 }, -- min and max width of the columns
+		spacing = 25, -- spacing between columns
+		align = "center", -- align columns left, center or right
+	},
+	ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+	show_help = true, -- show help message on the command line when the popup is visible
+	triggers = { "<leader>", "<localleader>", "g", "<Shift>", "[", "]" }, -- automatically setup triggers
+	-- triggers = {"<leader>"} -- or specify a list manually
+	triggers_blacklist = {
+		-- list of mode / prefixes that should never be hooked by WhichKey
+		-- this is mostly relevant for key maps that start with a native binding
+		-- most people should not need to change this
+		i = { "j", "k" },
+		v = { "j", "k" },
+	},
+})
 
 wk.register({ ["<leader>"] = keymap })
 wk.register({ ["<localleader>"] = local_keymap })
