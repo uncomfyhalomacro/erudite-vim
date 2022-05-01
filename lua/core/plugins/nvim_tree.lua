5c1b05bda6ce0ac4a16ec3ddd4c3d58e3304f355
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-05-01 20:19:32 +0800

    Remove deprecated settings/options of NvimTree

--- lua/core/plugins/nvim_tree.lua
+++ lua/core/plugins/nvim_tree.lua
@@ -1,6 +1,5 @@
 vim.g.nvim_tree_respect_buf_cwd = 1
 require("nvim-tree").setup({
-	open_on_startup = false,
 	ignore_ft_on_setup = { "alpha" },
 	update_cwd = true,
 	--respect_buf_cwd = 1,
@@ -20,7 +19,6 @@ require("nvim-tree").setup({
 	},
 	view = {
 		hide_root_folder = false,
-		auto_resize = true,
 		width = 45,
 		height = 30,
 		side = "left",
