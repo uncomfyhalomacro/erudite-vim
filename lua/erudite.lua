a117965feea206693988a48081e4a04b876502a1
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-06-05 17:37:13 +0800

    [fancy] update catppuccin

--- lua/erudite.lua
+++ lua/erudite.lua
@@ -1,6 +1,6 @@
 require("run_packer")
 require("core")
-vim.o.background = "light"
+-- vim.o.background = "dark"
 vim.cmd("colorscheme catppuccin")
 vim.g.transparent_enabled = false
 vim.g.presence_auto_update = true
