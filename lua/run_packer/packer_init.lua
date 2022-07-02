bcd08bee465d3e4b47e8a1cb7bdd0582233fbc0d
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-07-02 10:32:03 +0800

    [config] add nvim-surround

--- lua/run_packer/packer_init.lua
+++ lua/run_packer/packer_init.lua
@@ -71,6 +71,14 @@ local plugins = function(use)
 	use("tiagofumo/vim-nerdtree-syntax-highlight")
 
 	-- Utils
+	use({
+		"kylechui/nvim-surround",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	})
 	use("mfussenegger/nvim-dap")
 	use({
 		"Shatur/neovim-session-manager",
